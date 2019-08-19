b015121a7342286206cd7cd1e0d4270df6b64b32
Dan Finneran <daniel.finneran@gmail.com>
2019-08-19 18:24:36 +0100

    Backgrounding of process so that initial error checking works

--- cmd/automate.go
+++ cmd/automate.go
@@ -231,7 +231,7 @@ var plunderAutomateSSH = &cobra.Command{
 					err = deployment.FindDeployment(*deploymentName, *actionName, *host, *logFile, *resume)
 				} else {
 					// Parse the entire deployment
-					err = deployment.DeploySSH(*logFile, false)
+					err = deployment.DeploySSH(*logFile, false, false)
 				}
 				if err != nil {
 					log.Fatalf("%v", err)
@@ -322,7 +322,7 @@ var plunderAutomateVMware = &cobra.Command{
 					err = deployment.FindDeployment(*deploymentName, *actionName, *host, *logFile, *resume)
 				} else {
 					// Parse the entire deployment
-					err = deployment.DeploySSH(*logFile, false)
+					err = deployment.DeploySSH(*logFile, false, false)
 				}
 				if err != nil {
 					log.Fatalf("%v", err)
@@ -434,7 +434,7 @@ var plunderAutomateUI = &cobra.Command{
 			log.Fatalf("No Deployment information imported")
 		}
 
-		err := newMap.DeploySSH(*logFile, false)
+		err := newMap.DeploySSH(*logFile, false, false)
 		if err != nil {
 			log.Fatalf("%v", err)
 		}
